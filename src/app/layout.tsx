e4a915d593bd9a60085f7c2f7a1eccd5f702d4e1
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:27:20 +0530

    style: add h-full class to html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body>{children}</body>
     </html>
   );
