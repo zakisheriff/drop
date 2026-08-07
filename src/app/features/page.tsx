0e3456046b978be668c78c932563b3583ee2070e
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:39:38 +0530

    feat: add FeaturesPage function component

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -4,3 +4,14 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import FeaturesSection from "@/components/FeaturesSection";
 import Footer from "@/components/Footer";
+
+export default function FeaturesPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+      </main>
+      <Footer />
+    </>
+  );
+}
