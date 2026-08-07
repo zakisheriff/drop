10b11d4eb9f919715c16388e3151648f3e1257c1
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:39:38 +0530

    feat: add navbar downloadsection and footer imports

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,3 +1,6 @@
 "use client";
 
 import React from "react";
+import Navbar from "@/components/Navbar";
+import DownloadSection from "@/components/DownloadSection";
+import Footer from "@/components/Footer";
