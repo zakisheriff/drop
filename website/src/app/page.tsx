06bb8fb3b4934ff7c3e229f30b254ede06cc2b37
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 12:44:16 +0530

    feat: replace demo2.mp4 video file with demo3.mp4

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function RootPage() {
           <div className={styles.videoContainer}>
             <video
               className={styles.videoElement}
-              src="/demo2.mp4"
+              src="/demo3.mp4"
               autoPlay
               loop
               muted
