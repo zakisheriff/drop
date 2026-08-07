bea542dc084196b35bf2c681b704b0c543442a38
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:30:34 +0530

    style: add hero container div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,8 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="hero">
+      <div className="hero-container">
+      </div>
     </section>
   );
 }
