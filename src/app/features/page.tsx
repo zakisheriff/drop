986024cf5d88e9029dec6839384fc9e280632629
zakisheriff <zakisheriff@users.noreply.github.com>
2026-08-07 18:39:38 +0530

    feat: add navbar featuressection and footer imports

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,3 +1,6 @@
 "use client";
 
 import React from "react";
+import Navbar from "@/components/Navbar";
+import FeaturesSection from "@/components/FeaturesSection";
+import Footer from "@/components/Footer";
