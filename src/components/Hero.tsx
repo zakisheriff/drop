129d4232d5e5ac652f26a3cdf8f4c99b2e107b32
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:30:34 +0530

    style: add hero content wrapper div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,8 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="hero-container">
+        <div className="hero-content">
+        </div>
       </div>
     </section>
   );
