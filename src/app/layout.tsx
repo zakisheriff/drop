fcb84e0353246f362bc6c3cdd93056f3fbb6d67c
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:27:20 +0530

    feat: configure geist mono font with variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,3 +6,8 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
