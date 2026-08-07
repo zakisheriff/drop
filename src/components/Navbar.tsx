7575d5545c4959ef7803b419b12520c268c028d1
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:28:35 +0530

    style: add navbar container div

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav></nav>
+    <nav className="navbar">
+      <div className="navbar-container"></div>
+    </nav>
   );
 }
