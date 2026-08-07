603c09c67a7f950b82e65311a4d94cee145876f6
zakisheriff <zakisheriff@users.noreply.github.com>
2026-08-07 18:39:37 +0530

    style: add cta container div

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -5,6 +5,8 @@ import React from "react";
 export default function CTASection() {
   return (
     <section className="cta">
+      <div className="cta-container">
+      </div>
     </section>
   );
 }
