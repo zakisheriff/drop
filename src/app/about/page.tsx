a789197df6151ac449b216c1aed56870e60bcb9f
zakisheriff <zakisheriff@users.noreply.github.com>
2026-08-07 18:39:38 +0530

    feat: add AboutPage function component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,3 +3,14 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
+export default function AboutPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+      </main>
+      <Footer />
+    </>
+  );
+}
