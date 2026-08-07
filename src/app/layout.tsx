191c5c6d66eeca1a0fa91e466c108e76b6417c9c
zakisheriff <zakisheriff57@gmail.com>
2026-08-07 18:27:20 +0530

    feat: add font CSS variables to html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>{children}</body>
     </html>
   );
